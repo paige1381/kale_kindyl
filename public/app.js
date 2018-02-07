b0979baf83c09b5d2e392a895a78f662ccb5aa83
Paige Boyer <paige1381@gmail.com>
2018-02-06 20:12:40 -0500

    initial routing setup

--- public/app.js
+++ public/app.js
@@ -1,7 +1,36 @@
-const app = angular.module('kale-kindyl', []);
+const app = angular.module('kale-kindyl', ['ngRoute']);
 
 app.controller('MainController', ['$http', function($http) {
-
   this.menu = false;
+}])
+
+app.controller('HomeController', function() {
+
+})
+
+app.controller('AboutController', function() {
+
+})
+
+
+app.config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
+  $locationProvider.html5Mode({ enabled: true });
+
+  $routeProvider.when('/home', {
+    templateUrl: 'home.html',
+    controller: 'HomeController',
+    controllerAs: 'ctrl'
+  });
+
+  $routeProvider.when('/about', {
+  templateUrl: 'about.html',
+  controller: 'AboutController',
+  controllerAs: 'ctrl'
+});
+
+$routeProvider.otherwise({
+  redirectTo: '/home'
+})
+
 
 }])
