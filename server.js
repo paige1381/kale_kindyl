c455b8401f2894e6abff62537fdfc3294f9cab96
Paige Boyer <paige1381@gmail.com>
2018-02-28 17:57:54 -0500

    removed redirectRouter code

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const redirectRouter = (req, res) => {
-  res.sendFile("/", {root: "./public/"})
-}
+// const redirectRouter = (req, res) => {
+//   res.sendFile("/", {root: "./public/"})
+// }
 
 app.use(express.static('public'));
-app.use(redirectRouter);
+// app.use(redirectRouter);
 
 app.listen(PORT, () => console.log('site listening on port:', PORT));
